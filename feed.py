af2cc828b75bea34b4dbc8baf2ec1415bbeb9585
Brett Graves <148157697+BrettGraves-KAC@users.noreply.github.com>
2025-03-18 17:55:40 +0000

    update

--- feed.py
+++ feed.py
@@ -15,12 +15,12 @@ with open('feed.yaml', 'r') as file:
     xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
     xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
     xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
-    xml_tree.SubElement(channel_element, 'itunes: author').text = yaml_data['author']
-    xml_tree.SubElement(channel_element, 'descriptiom').text = yaml_data['description']
-    xml_tree.SubElement(channel_element, 'itunes: image', {'href': link_prefix + yaml_data['image']})
+    xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
+    xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
+    xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix + yaml_data['image']})
     xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
     xml_tree.SubElement(channel_element, 'link').text = link_prefix
-    xml_tree.SubElement(channel_element, 'itunes: category', {'text': yaml_data['category']})
+    xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
     
     for item in yaml_data['item']:
         item_element = xml_tree.SubElement(channel_element, 'item')
@@ -30,7 +30,7 @@ with open('feed.yaml', 'r') as file:
         xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
         xml_tree.SubElement(item_element, 'pubDate').text = item['published']
         
-        encolsure = xml_tree.SubElement(item_element, 'enclosure', {
+        enclosure = xml_tree.SubElement(item_element, 'enclosure', {
             'url': link_prefix + item['file'],
             'type': 'audio/mpeg',
             'length': item['length']
